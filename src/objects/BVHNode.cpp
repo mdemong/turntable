8d8098f6163354824c8fea6f9979196bd3348eab
mdemong <mdemong1@gmail.com>
2019-12-10 12:40:10 -0800

    implemented BVHNode intersect

--- src/objects/BVHNode.cpp
+++ src/objects/BVHNode.cpp
@@ -6,5 +6,33 @@ BVHNode::BVHNode()
 
 bool BVHNode::intersect(const Ray & ray, glm::vec3 & point, glm::vec3 & normal)
 {
-     return false;
+     glm::vec3 leftPt, leftNormal, rightPt, rightNormal;
+     bool bLeftHit = (left != NULL) && (left->intersect(ray, leftPt, leftNormal));
+     bool bRightHit = (right != NULL) && (right->intersect(ray, rightPt, rightNormal));
+     float leftDistSq = glm::distance2(leftPt, ray.p);     
+     float rightDistSq = glm::distance2(rightPt, ray.p);
+
+
+     if (bLeftHit && bRightHit) {
+          if (leftDistSq < rightDistSq) {
+               point = leftPt;
+               normal = leftNormal;
+          }
+          else {
+               point = rightPt;
+               normal = leftNormal;
+          }
+          return true;
+     }
+     else if (bLeftHit) {
+          point = leftPt;
+          normal = leftNormal;
+          return true;
+     }
+     else if (bRightHit) {
+          point = rightPt;
+          normal = rightNormal;
+          return true;
+     }
+     else return false;
 }
--- src/objects/BVHNode.h
+++ src/objects/BVHNode.h
@@ -1,8 +1,9 @@
 #pragma once
 #include "SceneObject.h"
+#include "box.h"
 
 // BVH tree
-// Algorithms and class design from Fundamentals of Computer Graphics, 4th ed.
+// Referenced from Fundamentals of Computer Graphics, 4th ed.
 
 
 class BVHNode :
@@ -12,8 +13,9 @@ public:
      BVHNode();
      void draw() {}
      bool intersect(const Ray &ray, glm::vec3 &point, glm::vec3 &normal) = 0;
+     // todo: pure virtual bbox()?
      SceneObject *left;
      SceneObject *right;
-     // todo: pure virtual bbox()?
+     Box bbox;
 };
 
